11f09966a4c664581edabdd36fc6e0d4f5fdb19f
sabbirpegon <sabbirwalton343@gmail.com>
2022-04-06 16:44:19 +0600

    complete menu

---
+++ menu.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <script src="https://kit.fontawesome.com/f7bd1b1106.js" crossorigin="anonymous"></script>
+
+
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Poppins:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/manu.css" />
+
+
+  <title>DIU CONNECTION</title>
+</head>
+
+<body>
+  <!-- Part Of navigation Bar -->
+  <section class="navmain">
+    <div class="nav">
+
+      <div class="navinner">
+        <a href="">
+          <div class="title">Diu Connection</div>
+        </a>
+
+        <div class="navouter" id="icon">
+          <ul class="naviconlist">
+
+            <li class="innericon">
+              <a href="#">
+                <i class="fa-solid fa-house-chimney fa-2x"></i>
+              </a>
+            </li>
+
+            <li class="innericon">
+              <a href="#">
+                <i class="fa-solid fa-file-circle-check fa-2x"></i>
+              </a>
+            </li>
+
+            <li class="innericon">
+              <a href="#">
+                <i class="fa-solid fa-clipboard-question fa-2x"></i>
+            </li>
+
+            <li class="innericon">
+              <a href="#">
+              <i class="fa-solid fa-user fa-2x"></i>
+              </a>
+            </li>
+            
+            <li class="innericon">
+              <a href="#">
+                <i class="fa-solid fa-bell fa-2x"></i>
+              </a>
+            </li>
+           
+
+
+
+
+          </ul>
+          </a>
+        </div>
+
+        <div class="navouter" id="search">
+          <label for="inputsearch" id="searchlable">
+
+            <i class="fa-solid fa-magnifying-glass fa-2x"></i>
+
+          </label>
+          <input type="search" name="search" placeholder="search" id="inputsearch">
+        </div>
+
+        <div class="navouter" id="profileid">
+          
+    
+          <button class="logout">Logout</button>
+        </div>
+
+      
+
+
+      </div>
+    </div>
+  </section>
+</body>
+
+</html>
